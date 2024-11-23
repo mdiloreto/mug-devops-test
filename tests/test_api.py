ee5b38a16a41c8084db7eacab8613c897f2a27dd
Mateo Di Loreto <mateoadiloreto@gmail.com>
2024-11-22 22:12:20 -0300

    Add testing to CI

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,11 @@ import requests
 ENDPOINT = os.getenv("TEST_ENDPOINT", "http://localhost:5000")
 AZURE_ENDPOINT = os.getenv("TEST_AZURE_ENDPOINT")
 AZURE_CREDENTIALS = os.getenv("TEST_AZURE_CREDENTIALS")
-URL = os.getenv("TEST_URL")
+URL = os.getenv("TEST_URL", "https://madsblog.net/2024/10/29/kubernetes-networking-parte-2/")
+
+# Ensure critical variables are set.
+if not AZURE_ENDPOINT or not AZURE_CREDENTIALS or not URL:
+    raise ValueError("Missing critical environment variables: AZURE_ENDPOINT, AZURE_CREDENTIALS, or URL")
 
 def test_translate_missing_body():
     response = requests.post(f"{ENDPOINT}/api/translate", json=None)
